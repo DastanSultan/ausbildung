80a0066c5ae804c0fc7066332e2e8659733a7920
Dastan Sultankulov <59560749+DastanSultan@users.noreply.github.com>
2023-02-07 15:29:10 +0100

    Add files via upload

---
+++ item/head.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/modal_min_css.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+    <link href='https://css.gg/heart.css' rel='stylesheet'>
+    <link href='https://css.gg/heart.css' rel='stylesheet'>
+    <title>Document</title>
+   <script src="js/jquery.js"></script>
+   <script src="js/jquery.modal.min.js"></script>
+   <script src="js/script.js"></script>
+</head>
