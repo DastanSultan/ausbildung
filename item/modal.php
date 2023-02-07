bdc5066e8f9687dd3db6978b9b5e8ecb5bd89523
Dastan Sultankulov <59560749+DastanSultan@users.noreply.github.com>
2023-02-07 15:30:25 +0100

    Add files via upload

---
+++ item/modal.php
@@ -0,0 +1,20 @@
+<!-- Окно для отправки сообщение -->
+<div class="modal_box">
+  <div class="modal_box_item">
+    <form action="">
+      <div class="close_sms_user"><div class="cloes_item"><span></span><span></span></div></div>
+      <div class="text_svyaz"><span>Связаться с пользователем</span></div>
+      <div class="sms_user">
+        <div class="sms_input"><input type="text" placeholder="Введите ваши сообщение" value=""></div>
+        <input type="text" id="id_tovar" value="">
+        <div class="sms_arte">
+          <div class="sms_arte_item" ttr="Ещё актуально?">Ещё актуально?</div>
+          <div class="sms_arte_item" ttr="Цена?">Цена?</div>
+          <div class="sms_arte_item" ttr="Обмен интересует?">Обмен интересует?</div>
+        </div>
+        <div class="user_sms_submit"><input type="submit" class="" value="Отправить"></div>
+      </div>
+    </form>
+  </div>
+</div>
+</div>
