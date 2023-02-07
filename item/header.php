5e08fef675d08703c1283480a89e52226bf153f0
Dastan Sultankulov <59560749+DastanSultan@users.noreply.github.com>
2023-02-07 15:27:11 +0100

    Create header.php

---
+++ item/header.php
@@ -0,0 +1,16 @@
+<div class="header">
+  <div class="logo_und_menu">
+    <div class="logo padding_15"><a href=""><span><img src="/img/logo.png" alt=""></span></a></div>
+  </div>
+  <div class="register_und_add">
+    <div class="hearth"><a href=""><span><img src="/img/icons8-shopping-cart-96.png" alt=""></span></a></div>
+    <div class="register">
+      <a href="#ex1" rel="modal:open">
+        <div class="register_item"><span>Eingeben</span><span>/</span><span>Anmelden</span></div>
+      </a>
+    </div>
+    <a href="add_tovar.php">
+      <div class="add_tovar"><span>Anzeige aufgeben</span></div>
+    </a>
+  </div>
+</div>
