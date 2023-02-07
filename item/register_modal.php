1ae4d33c73ca86a10325f0b305aecb72f2b8f117
Dastan Sultankulov <59560749+DastanSultan@users.noreply.github.com>
2023-02-07 15:32:33 +0100

    Add files via upload

---
+++ item/register_modal.php
@@ -0,0 +1,69 @@
+<div id="ex1" class="modal">
+  <div class="form_register">
+    <form action="">
+      <div class="text_register_form">
+        <div class="text_register_forms">Войти</div>
+        <div class="text_register_forms">Регистрация</div>
+      </div>
+      <div class="register_google">
+        <div class="google_reg"><a href=""><img src="/img/icons8-google-96.png" alt=""></a></div>
+        <div class="google_reg"><a href=""><img src="/img/icons8-facebook-96.png" alt=""></a></div>
+        <div class="google_reg"><a href=""><img src="/img/icons8-odnoklassniki-circled-96.png" alt=""></a></div>
+        <div class="google_reg"><a href=""><img src="/img/icons8-vk-circled-96.png" alt=""></a></div>
+      </div>
+      <div class="bottom_margin_reg">
+        <div class="input_register">
+          <div class="logo_input_register"><img src="/img/icons8-customer-100.png" alt=""></div>
+          <div class="input_item password_registers"><input maxlength="15" type="text" placeholder="Введите E-mail или телефон"></div>
+        </div>
+        <div class="message">Максимальное количество символов 15</div>
+      </div>
+      <div class="bottom_margin_reg">
+        <div class="input_register unlock_register">
+          <div class="logo_input_register"><img src="/img/icons8-lock-100.png" alt=""></div>
+          <div class="input_item password_registers"><input maxlength="12" type="password" placeholder="Пароль"></div>
+          <div class="unlock"><img src="/img/icons8-padlock-outline-96.png" alt=""></div>
+        </div>
+        <div class="message">Максимальное количество символов 12</div>
+      </div>
+      <div class="input_submit_register"><input type="submit" value="Войти"></div>
+    </form>
+  </div>
+  <div class="form_register_2">
+    <form action="">
+      <div class="text_register_form">
+        <div class="text_register_forms">Войти</div>
+        <div class="text_register_forms">Регистрация</div>
+      </div>
+      <div class="register_google">
+        <div class="google_reg"><a href=""><img src="/img/icons8-google-96.png" alt=""></a></div>
+        <div class="google_reg"><a href=""><img src="/img/icons8-facebook-96.png" alt=""></a></div>
+        <div class="google_reg"><a href=""><img src="/img/icons8-odnoklassniki-circled-96.png" alt=""></a></div>
+        <div class="google_reg"><a href=""><img src="/img/icons8-vk-circled-96.png" alt=""></a></div>
+      </div>
+      <div class="mak_lenght bottom_margin_reg">
+        <div class="input_register ">
+          <div class="logo_input_register"><span>+996</span></div>
+          <div class="input_item passy password_registers"><input id="passy" type="number" placeholder="XXX XXX XXX" minlength="8" maxlength="9"></div>
+        </div>
+        <div class="message">Максимальное количество символов 9</div>
+      </div>
+      <div class="bottom_margin_reg">
+        <div class="input_register unlock_register">
+          <div class="logo_input_register"><img src="/img/icons8-lock-100.png" alt=""></div>
+          <div class="input_item password_registers"><input maxlength="12" type="password" placeholder="Пароль"></div>
+          <div class="unlock"><img src="/img/icons8-padlock-outline-96.png" alt=""></div>
+        </div>
+        <div class="message">Максимальное количество символов 12</div>
+      </div>
+      <div class="checkbox_register">
+        <div id="checkbox_re"> <input type="checkbox" ></div>
+        <div class="check"> <span>Регистрируясь, вы принимаете <a href="">Пользовательское соглашение, Конфиденциальность</a> и <a href="">Договор-оферта</a>  на lalafo.kg</span>
+      </div>
+    </div>
+    <div class="input_submit_register">
+      <input type="submit" value="Войти">
+    </div>
+  </form>
+</div>
+</div>
